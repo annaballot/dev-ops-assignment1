f9202b3132055864e53a7ef4d2f464282e5cc2f7
Anna Ballot <annaballot@gmail.com>
2024-02-20 19:11:20 +0000

    Update details on the User data script on EC2

--- devops_1.py
+++ devops_1.py
@@ -15,26 +15,56 @@ import os
 #set variables
 url_image = "http://devops.witdemo.net/logo.jpg" 
 image_name = "logo.jpg"
+user_name = "aballot"
+ec2_key_name = 'abstudentkey'
+s3_file_upload = "index.html"
+ec2_security_group = "httpssh"
 
+print ("""---------------------------------------------------------------------------------
 
-print ("")
-print ("Running automated script to create, launch and monitor web servers")
-print ("")
+Running automated script to create, launch and monitor web servers
+...
+
+""")
 
 # datetime object containing current date and time
 now = datetime.now()
 dt_string = now.strftime("d%Y-%m-%dt%H-%M-%S")
-ec2_name = "ab-assignment-" + dt_string
-print("ec2 name: " + ec2_name)
 
-# random string to be used for bucket name
+# random string to be used for bucket name (https://docs.python.org/3/library/random.html)
 random_str = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
 
-s3_bucket_name = random_str + "-aballot"
+#set ec3 + s3 names
+ec2_name = user_name + "-" + dt_string
+s3_bucket_name = random_str + "-" + user_name
+print("---------------------------------------------------------------------------------")
+print("ec2 name: " + ec2_name)
 print("s3 bucket name: " + s3_bucket_name)
 print ("...")
 print ("")
 
+user_data_script = ""
+user_data_script = """#!/bin/bash
+            yum install httpd -y
+            systemctl enable httpd
+            systemctl start httpd
+            TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+            echo '<html>' > index.html
+            echo '<b>Instance ID: </b>' >> index.html
+            curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id >> index.html
+            echo '<br> <b>Instance Type: </b>' >> index.html
+            curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-type >> index.html
+            echo '<br> <b>Availability Zone:</b> ' >> index.html
+            curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone >> index.html
+            echo '<br> <b>Private IP address: </b>' >> index.html
+            curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4 >> index.html
+            echo '<br> <b>Security Groups: </b>' >> index.html
+            curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/security-groups >> index.html
+            echo '<br><br>Heres an image about S3 Buckets from the amazon website: <br>' >> index.html
+            echo '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWYcZPhTkco_M0M2sko4U4-eq-dUlL0XZmOF4UQqdgEA&s" alt="Amazon Image about S3"><br>' >> index.html
+            echo 'Hopefully it still exists on their site by the time Im finished this project!<br>' >> index.html
+            cp index.html /var/www/html/index.html"""
+            
 #create ec2 bucket
 ec2 = boto3.resource('ec2')
 new_instances = ec2.create_instances(
@@ -42,7 +72,7 @@ new_instances = ec2.create_instances(
     MinCount=1,
     MaxCount=1,
     InstanceType='t2.nano',
-    KeyName='abstudentkey',
+    KeyName=ec2_key_name,
     TagSpecifications=[
         {
             'ResourceType': 'instance',
@@ -55,38 +85,23 @@ new_instances = ec2.create_instances(
         },
     ],
     SecurityGroups=[
-        'httpssh',
+        ec2_security_group,
     ],
-    UserData="""#!/bin/bash
-            yum install httpd -y
-            systemctl enable httpd
-            systemctl start httpd
-            echo '<html>' > index.html
-            echo 'Private IP address: ' >> index.html
-            TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
-            curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4 >> index.html
-            cp index.html /var/www/html/index.html"""
+    UserData=user_data_script
     )
 
-#time.sleep(3) # Sleep for 3 seconds
-
+print("---------------------------------------------------------------------------------")
 print ("Created new EC2 instances:")
 print ("ID: ", new_instances[0].id)
 print ("Name: ", new_instances[0].tags[0]['Value'])
-print ("...")
 print ("")
 
 new_instances[0].wait_until_running()
 new_instances[0].reload() 
-print ("Instance is running")
-
-#print ("IP Address: ", new_instances[0].public_ip_address)
-
-#for inst in ec2.instances.all():
-#    print(inst.id, inst.state, inst.instance_type, inst.public_ip_address)
-
-
 inst = ec2.Instance(new_instances[0].id)
+
+print("---------------------------------------------------------------------------------")
+print ("Instance is running")
 print("TEST", inst.id, inst.state, inst.instance_type, inst.public_ip_address)
 print("http://"+inst.public_ip_address)
 
@@ -95,13 +110,12 @@ print("http://"+inst.public_ip_address)
 time.sleep(60)
 new_instances[0].reload() 
 ec2_url = "http://" + new_instances[0].public_ip_address
-print (ec2_url)
+print ("EC2 URL: " + ec2_url)
+
 webbrowser.open_new_tab(ec2_url)
 
 
 #creating an S3 bucket
-
-
 s3 = boto3.resource("s3")
 s3.create_bucket(Bucket=s3_bucket_name) 
 
@@ -130,7 +144,7 @@ website_configuration = {
     'IndexDocument': {'Suffix': 'index.html'},
 }
 
-bucket_website = s3.BucketWebsite(s3_bucket_name)   # replace with your bucket name or a string variable
+bucket_website = s3.BucketWebsite(s3_bucket_name)
 
 response = bucket_website.put(WebsiteConfiguration=website_configuration)
 
@@ -149,9 +163,7 @@ if response.status_code == 200:
 else:
     print(f"Failed to download image from {image_url}. Status code: {response.status_code}")
     
-#put image in object
-object_name = "index.html"
-
+#create html file for s3 bucket
 html_data = f"""
 	<!doctype html>
 	<html>
@@ -162,21 +174,25 @@ html_data = f"""
 
 	    <body>
 		This is a test file!  If you can see this, your index.html is working!
-		<img src=""" + image_name + """ alt="Assignment 1 Image">
+	    </body>
+	    <body>
+	        <img src=""" + image_name + """ alt="Logo Image Assignment 1">
 	    </body>
 	</html>
     """
     
-    
+#upload html file + image to s3 bucket 
+#reference: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/put_object.html   
 try:
-    #response = s3.Object(s3_bucket_name, object_name).put(Body=open(object_name, 'rb'))
-    s3client.put_object(Bucket=s3_bucket_name, Key=object_name, Body=html_data.encode('utf-8'), ContentType='text/html')
+    s3client.put_object(Bucket=s3_bucket_name, Key=s3_file_upload, Body=html_data.encode('utf-8'), ContentType='text/html')
     with open(image_name, 'rb') as file:
             s3client.put_object(Bucket=s3_bucket_name, Key=image_name, Body=file, ContentType='image/jpeg')
 except Exception as error:
     print (error)    
     
+print ("http://" + s3_bucket_name + ".s3-website-us-east-1.amazonaws.com/" + s3_file_upload)
 
-print ("http://" + s3_bucket_name + ".s3-website-us-east-1.amazonaws.com/" + object_name)
+#launching a browser
+webbrowser.open_new_tab("http://" + s3_bucket_name + ".s3-website-us-east-1.amazonaws.com/" + s3_file_upload)
 
 
